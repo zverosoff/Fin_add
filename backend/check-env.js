6cfd13d364096f4fa81e054a45e421efed2e4963
zverosoff <raven-udomlya@mail.ru>
2026-09-22 19:13:25 +0300

    deploy: prepare for Amvera

--- backend/check-env.js
+++
@@ -1,6 +0,0 @@
-import 'dotenv/config';
-
-console.log('JWT_SECRET:', process.env.JWT_SECRET?.slice(0, 10) + '…');
-console.log('USER_SERGEY_PIN:', JSON.stringify(process.env.USER_SERGEY_PIN));
-console.log('USER_SASHA_PIN:', JSON.stringify(process.env.USER_SASHA_PIN));
-console.log('PORT:', process.env.PORT);
